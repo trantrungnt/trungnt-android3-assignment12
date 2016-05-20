3aad9125e0863b372c001317752afddb5ae966ba
Trần Trung <nguyentrantrung1986@gmail.com>
2016-05-21 03:40:16 +0700

    use Async Task XML Parse (temp)

---
+++ ServiceXMLParse/app/src/main/java/techkids/mad3/servicexmlparse/ItemDetailActivity.java
@@ -0,0 +1,30 @@
+package techkids.mad3.servicexmlparse;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+
+/**
+ * Created by TrungNT on 5/21/2016.
+ */
+public class ItemDetailActivity extends AppCompatActivity {
+    private WebView webViewNews;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_detail);
+        initComponent();
+    }
+
+    private void initComponent()
+    {
+        webViewNews = (WebView) this.findViewById(R.id.webViewNews);
+        webViewNews.getSettings().setJavaScriptEnabled(true);
+
+        Bundle bundle = getIntent().getExtras();
+        String urlDescription = bundle.getString("urlDescription");
+        webViewNews.loadUrl(urlDescription);
+    }
+}
+
---
+++ ServiceXMLParse/app/src/main/java/techkids/mad3/servicexmlparse/ItemListActivity.java
@@ -0,0 +1,236 @@
+package techkids.mad3.servicexmlparse;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+import com.squareup.picasso.Picasso;
+
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.List;
+
+/**
+ * An activity representing a list of Items. This activity
+ * has different presentations for handset and tablet-size devices. On
+ * handsets, the activity presents a list of items, which when touched,
+ * item details. On tablets, the activity presents the list of items and
+ * item details side-by-side using two vertical panes.
+ */
+public class ItemListActivity extends AppCompatActivity {
+
+    /**
+     * Whether or not the activity is in two-pane mode, i.e. running on a tablet
+     * device.
+     */
+    private boolean mTwoPane;
+    List<VnExpressXmlParser.Item> items = null;
+    RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_list);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle(getTitle());
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        recyclerView = (RecyclerView) findViewById(R.id.item_list);
+        setupRecyclerView();
+
+//        if (findViewById(R.id.item_detail_container) != null) {
+//            // The detail container view will be present only in the
+//            // large-screen layouts (res/values-w900dp).
+//            // If this view is present, then the
+//            // activity should be in two-pane mode.
+//            mTwoPane = true;
+//        }
+
+    }
+
+    private void setupRecyclerView() {
+        new DownloadXmlTask().execute("http://vnexpress.net/rss/tin-moi-nhat.rss");
+    }
+
+    public class SimpleItemRecyclerViewAdapter
+            extends RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder> {
+
+        private final List<VnExpressXmlParser.Item> mValues;
+
+        public SimpleItemRecyclerViewAdapter(List<VnExpressXmlParser.Item> items) {
+            mValues = items;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_list_content, parent, false);
+            if (null == view) {
+                Log.d("XMLL", "view = null");
+            }
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(final ViewHolder holder, final int position) {
+            holder.mItem = mValues.get(position);
+            if (holder.title == null) {
+                Log.d("XMLL", "" + mValues.get(position).title);
+                Log.d("XMLL", "" + mValues.get(position).description);
+                Log.d("XMLL", "" + mValues.get(position).pubDate);
+
+            }
+
+            String description = mValues.get(position).description;
+
+            holder.title.setText(mValues.get(position).title);
+            holder.description.setText(description);
+            holder.pubDate.setText(mValues.get(position).pubDate);
+
+            //tim link anh jpg va cat chuoi, sau do load anh jpg voi thu vien Picasso
+            int startDescription = description.indexOf("src=\"http://");
+            int endDescription = description.indexOf(".jpg");
+            final String urlImage = description.substring(startDescription + 5, endDescription + 4);
+            Picasso.with(holder.image.getContext()).load(urlImage).into(holder.image);
+
+            holder.mView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+//                    if (mTwoPane) {
+//                        Bundle arguments = new Bundle();
+//                        arguments.putString(ItemDetailFragment.ARG_ITEM_ID, holder.mItem.id);
+//                        ItemDetailFragment fragment = new ItemDetailFragment();
+//                        fragment.setArguments(arguments);
+//                        getSupportFragmentManager().beginTransaction()
+//                                .replace(R.id.item_detail_container, fragment)
+//                                .commit();
+//                    } else {
+//                        Context context = v.getContext();
+//                        Intent intent = new Intent(context, ItemDetailActivity.class);
+//                        intent.putExtra(ItemDetailFragment.ARG_ITEM_ID, holder.mItem.id);
+//
+//                        context.startActivity(intent);
+//                    }
+                    Context context = v.getContext();
+                    Intent intent = new Intent(context, ItemDetailActivity.class);
+                    intent.putExtra("urlDescription", mValues.get(position).link);
+                    context.startActivity(intent);
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return mValues.size();
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            public final View mView;
+            public final ImageView image;
+            public final TextView title;
+            public final TextView description;
+            public final TextView pubDate;
+            public VnExpressXmlParser.Item mItem;
+
+            public ViewHolder(View view) {
+                super(view);
+                mView = view;
+                image = (ImageView) view.findViewById(R.id.image);
+                title = (TextView) view.findViewById(R.id.title);
+                description = (TextView) view.findViewById(R.id.description);
+                pubDate = (TextView) view.findViewById(R.id.pubDate);
+            }
+        }
+    }
+
+    // Implementation of AsyncTask used to download XML feed from stackoverflow.com.
+    private class DownloadXmlTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                return loadXmlFromNetwork(urls[0]);
+            } catch (IOException e) {
+                return getResources().getString(R.string.connection_error);
+            } catch (XmlPullParserException e) {
+                return getResources().getString(R.string.xml_error);
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            recyclerView.setAdapter(new SimpleItemRecyclerViewAdapter(items));
+        }
+    }
+
+    // Uploads XML from stackoverflow.com, parses it, and combines it with
+// HTML markup. Returns HTML string.
+    private String loadXmlFromNetwork(String urlString) throws XmlPullParserException, IOException {
+        InputStream stream = null;
+        // Instantiate the parser
+        VnExpressXmlParser vnExpressXmlParser = new VnExpressXmlParser();
+        String title = null;
+        String url = null;
+        String summary = null;
+        Calendar rightNow = Calendar.getInstance();
+        DateFormat formatter = new SimpleDateFormat("MMM dd h:mmaa");
+
+        try {
+            stream = downloadUrl(urlString);
+            items = vnExpressXmlParser.parse(stream);
+            // Makes sure that the InputStream is closed after the app is
+            // finished using it.
+        } finally {
+            if (stream != null) {
+                stream.close();
+            }
+        }
+
+        return null;
+    }
+
+    // Given a string representation of a URL, sets up a connection and gets
+// an input stream.
+    private InputStream downloadUrl(String urlString) throws IOException {
+        URL url = new URL(urlString);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setReadTimeout(10000 /* milliseconds */);
+        conn.setConnectTimeout(15000 /* milliseconds */);
+        conn.setRequestMethod("GET");
+        conn.setDoInput(true);
+        // Starts the query
+        conn.connect();
+        return conn.getInputStream();
+    }
+}
